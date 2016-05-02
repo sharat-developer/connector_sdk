8af9244beed4d7dbc268004772c61b7bcece2d26
Sim Ee Shan <eeshansim@gmail.com>
2016-05-02 12:26:46 -0700

    fix output data

--- basic_auth/unbounce_connector.rb
+++ basic_auth/unbounce_connector.rb
@@ -32,15 +32,57 @@
   },
   
   object_definitions: {
+
     form: {
       fields: ->(connection) {
         get("https://api.unbounce.com/pages/#{connection['page_id']}/form_fields")['formFields'].
           map { |field| { name: field['id'] } }
       }
+    },
+
+    page: {
+      fields: ->(connection) {
+        [
+          { name:'subAccountId', type: :integer },
+          { name:'integrationsCount', type: :integer },
+          { name:'integrationsErrorsCount', type: :integer },
+          { name:'id' },
+          { name:'createdAt', type: :datetime },
+          { name:'lastPublishedAt', type: :datetime },
+          { name:'name' },
+          { name:'state' },
+          { name:'url', control_type: 'url' },
+          { name:'variantsCount', type: :integer },
+          { name:'domain', control_type: 'url' },
+          { name:'fullUrl', control_type: 'url' },
+          { name: 'metaData', type: :object, properties: [
+            { name: 'documentation', control_type: 'url' },
+            { name: 'location', control_type: 'url' },
+            { name: 'related', type: :object, properties: [
+              { name: 'leads', control_type: 'url' },
+              { name: 'subAccount', control_type: 'url' }
+            ]}
+          ]}
+        ]
+      }
+    }
+  },
+
+  actions: {
+
+    get_page_details: {
+      input_fields: ->() {},
+      execute: ->(connection,input) {
+        get("https://api.unbounce.com/pages/#{connection['page_id']}")
+      },
+      output_fields: ->(object_definitions) {
+        object_definitions['page']
+      }
     }
   },
   
   triggers: {
+
     new_submission: {
       
       type: :paging_desc,
@@ -60,6 +102,14 @@
                          sort_order: :desc, # API sorts by creation date
                          limit: 10)['leads']
 
+        leads.map do |lead|
+          lead['formData'] = lead['formData'].map do |k,v|
+                               { k => v.first }
+                             end.
+                             inject(:merge)
+          lead
+        end
+
         next_updated_since = leads.first['createdAt'] unless leads.length == 0
 
         {
@@ -70,12 +120,10 @@
 
       output_fields: ->(object_definitions) {
         [
-          { name: 'leads', type: :array, of: :object, properties: [
-            { name: 'id', type: :integer },
-            { name: 'form_data', type: :array, of: :object, properties: object_definitions['form'] }
-          ]}
+          { name: 'id', type: :integer },
+          { name: 'formData', type: :object, properties: object_definitions['form'] }
         ]
       }
     }
-  }
+  },
 }
