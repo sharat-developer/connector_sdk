41c357b05e3719014d82f7f6ee82f0ddb07ab695
Adam Goh <adamwgoh@gmail.com>
2016-06-02 11:25:55 +0800

    Fix indentation and spacing

--- oauth2/podio_connector.rb
+++ oauth2/podio_connector.rb
@@ -15,9 +15,9 @@
         'https://podio.com/oauth/token'
       },
 
-      client_id: 'workato-wwzue9',
+      client_id: 'YOUR_PODIO_CLIENT_ID',
 
-      client_secret: 'zCo8L8p4KZXm5bhse5U7PFo12NjiZjHu1XiTVV7E76SxoskqKmlU6g5ZOava7I05',
+      client_secret: 'YOUR_PODIO_CLIENT_SECRET',
 
       credentials: ->(connection, access_token) {
         headers('Authorization': "OAuth2 #{access_token}")
@@ -27,48 +27,49 @@
 
   object_definitions: {
     contact: {
-			fields: ->(connection) {
-        [
-          { name: 'profile_id'},
-          { name: 'name' },
-          { name: 'organization' },
-          { name: 'department' },
-          { name: 'skype' },
-          { name: 'about' },
-          { name: 'address' },
-          { name: 'zip' },
-          { name: 'city' },
-          { name: 'state' },
-          { name: 'country' },
-          { name: 'mail' },
-          { name: 'phone' },
-          { name: 'title' },
-          { name: 'linkedin' },
-          { name: 'url' },
-          { name: 'twitter' }
-        ]
+	fields: ->(connection) {
+	        [
+	          { name: 'profile_id'},
+	          { name: 'name'},
+	          { name: 'organization'},
+	          { name: 'department'},
+	          { name: 'skype'},
+	          { name: 'about'},
+	          { name: 'address'},
+	          { name: 'zip'},
+	          { name: 'city'},
+	          { name: 'state'},
+	          { name: 'country'},
+	          { name: 'mail'},
+	          { name: 'phone'},
+	          { name: 'title'},
+	          { name: 'linkedin'},
+	          { name: 'url'},
+	          { name: 'twitter'}
+	        ]
       },
     },
 
     application: {
       fields: ->(connection)	{
         [
-          {	name: 'app_id'	},
-          {	name: 'original'	},
-          {	name: 'original_revision'	},
-          {	name: 'status'	},
-          {	name: 'space_id'	},
-          {	name: 'owner', type: :object, properties: [{name: 'user_id', type: :integer}, {name: 'avatar'}, {name: 'name'}] 	},
-          {	name: 'link'	},
-          {	name: 'link_add'	},
-          {	name: 'config', type: :object,
-            properties: [
-              {	name: 'type', control_type: 'select', picklist: [['standard'], ['meeting'], ['contact']] },
+          { name: 'app_id'},
+          { name: 'original'},
+          { name: 'original_revision'},
+          { name: 'status'},
+          { name: 'space_id'},
+          { name: 'owner', type: :object, properties: [{name: 'user_id', type: :integer}, {name: 'avatar'}, {name: 'name'}]},
+          { name: 'link'},
+          { name: 'link_add'},
+          { name: 'config', type: :object,
+            properties: 
+            [
+              {	name: 'type', control_type: 'select', picklist: [['standard'], ['meeting'], ['contact']]},
               {	name: 'name'},
               {	name: 'item_name'},
-              {	name:	'description'},
-              {	name:	'usage'},
-              {	name:	'external_id'},
+              {	name: 'description'},
+              {	name: 'usage'},
+              {	name: 'external_id'},
               {	name: 'icon'},
               {	name: 'allow_edit', type: :boolean},
               {	name: 'default view'},
@@ -84,14 +85,14 @@
               {	name: 'yesno_label'},
               {	name: 'tasks'}
             ]
-        	}
+          }
         ]
       }
     },
 
     tag: {
       fields: ->(connection){
-        [{	name: 'count'},	{	name: 'text'}]
+        [{ name: 'count'},{ name: 'text'}]
       }
     }
   },
@@ -101,7 +102,7 @@
     get_apps: {
     	input_fields: ->(object_definitions){
         [
-          {name: 'Workspace ID', optional: 'false' }
+          { name: 'Workspace ID', optional: 'false'}
         ]
       },
 
@@ -110,57 +111,55 @@
       },
 
       output_fields: ->(object_definitions) {
-					{	name: 'applications', type: 'array', of: 'object', properties: object_definitions['application']}
+	{ name: 'applications', type: 'array', of: 'object', properties: object_definitions['application']}
       }
     },
 
     get_app_detail: {
     	input_fields: ->(object_definitions){
         [
-        	{	name: 'Application ID', optional: 'false'}
+         { name: 'Application ID', optional: 'false'}
         ]
       },
 
       execute: ->(connection, input) {
-        	{	app_detail: get("https://api.podio.com/app/#{input['Application ID']}")}
+      	{ app_detail: get("https://api.podio.com/app/#{input['Application ID']}")}
       },
 
       output_fields: ->(object_definitions){
         object_definitions['application']
       }
-   	},
+   },
 
-    get_app_tags: {
-    	input_fields: ->(object_definitions){
-        [
-        	{	name: 'Application ID', optional: 'false'}
-        ]
-      },
+   get_app_tags: {
+   	input_fields: ->(object_definitions){
+         [
+           { name: 'Application ID', optional: 'false'}
+         ]
+      	},
 
-      execute: ->(connection, input) {
-        	{	tags: get("https://api.podio.com/tag/app/#{input['Application ID']}")}
-      },
+	execute: ->(connection, input) {
+	   { tags: get("https://api.podio.com/tag/app/#{input['Application ID']}")}
+	},
 
-      output_fields: ->(object_definitions){
-				object_definitions['tag']
-      }
-   	},
+	output_fields: ->(object_definitions){
+	   object_definitions['tag']
+	}
+   },
 
-    get_contacts: {
-      input_fields: ->(object_definitions) {
-        []
-      },
+   get_contacts: {
+	input_fields: ->(object_definitions) {[]},
 
-      execute: ->(connection, input) {
-        { contacts: get("https://api.podio.com/contact/?limit=5") }
-      },
+	execute: ->(connection, input) {
+	   { contacts: get("https://api.podio.com/contact/?limit=5")}
+	},
 
-      output_fields: ->(object_definitions) {
-        [
-          { name: 'contacts', type: 'array', of: 'object', properties: object_definitions['contact'] }
-      	]
-      }
-    }
+	output_fields: ->(object_definitions) {
+         [
+	   { name: 'contacts', type: 'array', of: 'object', properties: object_definitions['contact']}
+      	 ]
+      	}
+   }
 
   }
 }
