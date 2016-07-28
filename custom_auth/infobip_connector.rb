48bd04800f777780ebcc9e4c853d018b00173528
jantolis <josip.antolis@infobip.com>
2016-07-28 16:19:49 +0200

    Switched to using logs api endpoint for sms_received trigger

--- custom_auth/infobip_connector.rb
+++ custom_auth/infobip_connector.rb
@@ -17,7 +17,7 @@
         }
     },
     test: ->(connection) {
-      get("https://api.infobip.com/sms/1/logs").params(limit: 1)
+      get('https://api.infobip.com/sms/1/logs').params(limit: 1)
     },
     object_definitions: {
         send_sms_request: {
@@ -54,7 +54,8 @@
                   {name: 'text'},
                   {name: 'cleanText'},
                   {name: 'keyword'},
-                  {name: 'smsCount', type: :integer}
+                  {name: 'smsCount', type: :integer},
+                  {name: 'receivedAt', type: :datetime}
               ]
             }
         }
@@ -65,7 +66,7 @@
               object_definitions['send_sms_request'].required('to')
             },
             execute: ->(connection, input) {
-              post("https://api.infobip.com/sms/1/text/single", input)['send_sms_request']
+              post('https://api.infobip.com/sms/1/text/single', input)['send_sms_request']
             },
             output_fields: ->(object_definitions) {[
                 { name: 'messages', type: :array, of: :object, properties: object_definitions['sent_sms_info'] }
@@ -74,16 +75,16 @@
     },
     triggers: {
         sms_received: {
+            poll: ->(connection, input, last_received_at) {
+              received_since = (last_received_at || Time.now - 30 * 60).utc.strftime('%Y-%m-%dT%H%%3A%M%%3A%S%%2E%L%%2B00%%3A00')
 
-            poll: ->(connection, input, last_received_since) {
+              received_messages = get("https://api.infobip.com/sms/1/inbox/logs?receivedSince=#{received_since}")
 
-              page_size = 100
-              received_messages = get("https://api.infobip.com/sms/1/inbox/reports").params(limit: page_size)
               received_sms_info = received_messages['results']
+              last_received_at = received_sms_info.length == 0 ? received_since : received_sms_info[0]['receivedAt']
               {
-                  events: received_sms_info,
-                  next_poll: nil,
-                  can_poll_more: received_sms_info.length == page_size
+                  events: received_sms_info.reverse,
+                  next_poll: last_received_at
               }
             },
             dedup: ->(received_sms_info) {
