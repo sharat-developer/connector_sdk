49735a56c77794746a774d9cc1c6cfa69c213a2f
rameshlk <ramesh.ramz@gmail.com>
2016-08-13 08:45:37 +0530

    Added airbrake connector

---
+++ api_key_auth/airbrake_connector.rb
@@ -0,0 +1,73 @@
+{
+  title: 'Airbrake',
+
+  connection: {
+    fields: [
+      {
+        name: 'api_key',
+        control_type: 'password'
+      }
+    ],
+
+    authorization: {
+      type: 'api_key',
+
+      credentials: ->(connection) {
+        params(api_key: connection['api_key'])
+      }
+    }
+  },
+  
+  test: ->(connection) {
+    get("https://airbrake.io/api/v3/projects?key=#{connection['api_key']}")
+  },
+
+  object_definitions: {
+    project: {
+        fields: ->() {
+        [ 
+          {name: 'id'},
+          {name: 'name'},
+          {name: 'deployId',type: :integer},
+          {name: 'deployAt',type: :timestamp},
+          {name: 'noticeTotalCount',type: :integer},
+          {name: 'rejectionCount',type: :integer},
+          {name: 'fileCount',type: :integer},
+          {name: 'deployCount',type: :integer},
+          {name: 'groupResolvedCount',type: :integer},
+          {name: 'groupUnresolvedCount',type: :integer}
+       ]
+     }
+    }
+   },
+  
+  actions: {
+   
+   get_project: {
+      
+    description: 'Get<span class="provider">project</span> in <span class="provider">airbrake</span>',
+     
+    input_fields: ->(){
+      
+      [{name: 'id',hint: 'Enter your project ID',optional: false}]
+      },
+     
+      execute: ->(connection, input) {
+        get("https://airbrake.io/api/v3/projects/#{input['id']}",input)
+      },
+     
+      
+    output_fields: ->(object_definitions){[
+      
+     {name: 'project',type: :object,properties:[
+      {name: 'id',type: :integer},
+      {name: 'name'},
+      {name: 'throttles',type: :boolean},
+      {name: 'rateLimited',type: :boolean}]}
+
+      ]
+      }
+     }
+    }
+   }
+
