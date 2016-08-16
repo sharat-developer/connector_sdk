6912a3014455b7e6d5288c3ce5ba5ac61a7cea66
Nivedita Rajendiran <nivi@workato.com>
2016-08-16 10:49:23 -0700

    updated with latest merged code

--- oauth2/re_nxt_connector.rb
+++ oauth2/re_nxt_connector.rb
@@ -13,38 +13,33 @@
         'https://oauth2.sky.blackbaud.com/token'
       },
 
-      client_id: 'YOUR_RE_NXT_CLIENT_ID',
-
-      client_secret: 'YOUR_RE_NXT_CLIENT_SECRET',
-
-      credentials: ->(connection, access_token) {
-        headers('bb-api-subscription-key': "YOUR_BB_API_SUBSCRIPTION_KEY", 'Authorization': "Bearer #{access_token}")
+      credentials: ->(_connection, access_token) {
+        headers('bb-api-subscription-key': AppSettings.oauth2.raisers_edge.api_subscription_key,
+                'Authorization': "Bearer #{access_token}")
       }
     }
   },
-  
+
   object_definitions: {
+
     constituent: {
       fields: ->() {
         [
-          { name: "id" },
+          { name: "id", hint: "Constituent ID", optional: false },
           { name: "type" },
           { name: "lookup_id" },
           { name: "inactive", type: :boolean },
-          { name: "name" },
-          { name: "last" },
-          { name: "first" },
-          { name: "middle" },
-          { name: "preferred_name" },
-          { name: "title" },
-          { name: "gender" },
-          { name: "birthdate", type: :object, properties: [
-            { name: "y", type: :integer },
-            { name: "m", type: :integer },
-            { name: "d", type: :integer }]
-          },
+          { name: 'name', hint: 'For organizations only' },
+          { name: 'last', hint: 'Required for individual. All fields change on update.' },
+          { name: 'first', hint: 'For individuals only. All fields change on update.' },
+          { name: 'middle', hint: 'For individuals only. All fields change on update.' },
+          { name: 'preferred_name', hint: 'For individuals only. All fields change on update.' },
+          { name: 'title', hint: 'For individuals only. All fields change on update.', control_type: 'select', pick_list: 'title' },
+          { name: 'suffix', hint: 'For individuals only. All fields change on update.', control_type: 'select', pick_list: 'suffix' },
+          { name: "gender", hint: 'For individuals only. All fields change on update.', control_type: 'select', pick_list: 'gender' },
+          { name: "birthdate", type: :date },
           { name: "age", type: :integer },
-          { name: "deceased", type: :boolean },
+          { name: "deceased", type: :boolean, hint: 'For individuals only. All fields change on update.' },
           { name: "address", type: :object, properties: [
             { name: "id" },
             { name: "constituent_id" },
@@ -62,14 +57,14 @@
           { name: "email", type: :object, properties: [
             { name: "id" },
             { name: "type" },
-            { name: "address" },
+            { name: "address", control_type: 'email' },
             { name: "do_not_email" },
             { name: "primary", type: :boolean },
             { name: "inactive", type: :boolean }]
           },
           { name: "phone", type: :object, properties: [
             { name: "id" },
-            { name: "type" },
+            { name: "type", control_type: 'phone' },
             { name: "number" },
             { name: "do_not_call", type: :boolean },
             { name: "primary", type: :boolean },
@@ -91,170 +86,152 @@
           { name: "date_modified" },
         ]
       }
-    },
-    email: {
-      fields: ->() {
-        [
-          { name: "id" },
-          { name: "constituent_id" },
-          { name: "type" },
-          { name: "address" },
-          { name: "do_not_email", type: :boolean },
-          { name: "primary", type: :boolean },
-          { name: "inactive", type: :boolean }]
-      }
-    }, 
-    phone: {
-      fields: ->() {
-        [
-          { name: "id" },
-          { name: "constituent_id" },
-          { name: "type" },
-          { name: "number" },
-          { name: "do_not_call", type: :boolean },
-          { name: "primary", type: :boolean },
-          { name: "inactive", type: :boolean }]
-      }
-    },
-    address: {
-      fields: ->() {
-        [
-          { name: "id" },
-          { name: "constituent_id" },
-          { name: "type" },
-          { name: "formatted_address" },
-          { name: "preferred", type: :boolean },
-          { name: "do_not_mail", type: :boolean },
-          { name: "address_lines" },
-          { name: "city" },
-          { name: "state" },
-          { name: "postal_code" },
-          { name: "country" }]
-      }
-    },
+    }
   },
 
   actions: {
     update_constituent: {
-      input_fields: ->() {
-        [ { name: 'constituent_ID', type: :integer, optional: false },
-          { name: 'name', hint: 'For organizations only' },
-          { name: 'last', hint: 'Required for individual. All fields change on update.'},
-          { name: 'first', hint: 'For individuals only. All fields change on update.'},
-          { name: 'middle', hint: 'For individuals only. All fields change on update.'},
-          { name: 'preferred_name', hint: 'For individuals only. All fields change on update.' },
-          { name: 'title', hint: 'For individuals only. All fields change on update.', control_type: 'select', pick_list: 'title' },
-          { name: 'suffix', hint: 'For individuals only. All fields change on update.', control_type: 'select', pick_list: 'suffix' },         
-          { name: 'gender', hint: 'For individuals only. All fields change on update.', control_type: 'select', pick_list: [
-            ["Male","male"],
-            ["Female","female"],
-            ["Unknown","unknown"]]},
-          { name: "birthdate", hint: 'For individuals only. All fields change on update.', type: :date},
-          { name: 'deceased', hint: 'For individuals only. All fields change on update.', control_type: 'select', pick_list: [
-            ["true", true],
-            ["false", false]]}
-        ]
+      description: "Update <span class='provider'>constituent</span> in <span class='provider'>Raiser's Edge NXT</span>",
+
+      input_fields: ->(object_definitions) {
+        object_definitions['constituent'].only("id", "name", "last", "first", "middle", "preferred_name",
+                                               "suffix", "title", "gender", "birthdate", "deceased")
       },
 
-      execute: ->(connection, input) {
-        birthdate = input['birthdate'].to_s.split("-")
-        if input['name'] != nil
-          info = {"name"=>input['name']}
-        else
-          if (input['last'].present? || input['first'].present? || input['middle'].present? || input['preferred_name'].present? || input['title'].present? || input['suffix'].present? || input['gender'].present? || input['birthdate'].present? || input['deceased'].present?)
-            info = {"last"=>input['last'], "first"=>input['first'], "middle"=>input['middle'], "preferred_name"=>input['preferred_name'], "title"=>input['title'], "suffix"=>input['suffix'], "gender"=>input['gender'], "birthdate"=>{"y"=>birthdate[0], "m"=>birthdate[1], "d"=>birthdate[2]}, "deceased"=>input['deceased']}
-          else
-            info = {}
-          end
+      execute: ->(_connection, input) {
+        if input['birthdate'].present?
+          birthdate = input['birthdate'].to_date
+          date_hash = {
+            'y' => birthdate.year.to_i,
+            'm' => birthdate.mon.to_i,
+            'd' => birthdate.mday.to_i
+          }
+          input['birthdate'] = date_hash
         end
-        patch("https://api.sky.blackbaud.com/constituent/v1/constituents/#{input['constituent_ID']}", info)
+
+        payload = input.reject { |k,v| k == "id" }
+
+        patch("https://api.sky.blackbaud.com/constituent/v1/constituents/#{input['id']}", payload)
       },
 
-      output_fields: ->(object_definitions) {
-      }
+      output_fields: ->(_object_definitions) {}
     },
 
     search_constituent: {
-      input_fields: ->() {
-        [ { name: 'search_text', optional: false , hint: "Searches first/middle/last names and email addresses"} ]
+      description: "Search <span class='provider'>constituent</span> in <span class='provider'>Raiser's Edge NXT</span>",
+
+      input_fields: ->(_object_definitions) {
+        [
+          { name: 'search_text', optional: false, hint: "Searches first/middle/last names and email addresses" }
+        ]
       },
-      execute: ->(connection, input) {
+
+      execute: ->(_connection, input) {
+        url = "https://api.sky.blackbaud.com/constituent/v1/constituents/search?searchText=#{input['search_text']}"
+
         {
-        'constituents': get("https://api.sky.blackbaud.com/constituent/v1/constituents/search?searchText=#{input['search_text']}")['value']
+          'constituents': get(url)['value']
         }
       },
-      output_fields: ->(object_definitions) {
-        [{
-          name: 'constituents',
-          type: :array, of: :object,
-          properties: [
-            { name: "id", type: :integer},
-            { name: "name" },
-            { name: "address" },
-            { name: "email" }]
-        }]
+
+      output_fields: ->(_object_definitions) {
+        [
+          {
+            name: 'constituents', type: :array, of: :object,
+            properties: [
+              { name: "id", type: :integer },
+              { name: "name" },
+              { name: "address" },
+              { name: "email", control_type: 'email' }
+            ]
+          }
+        ]
       }
     }
   },
-  
+
   triggers: {
-    
+
     new_constituent: {
+      description: "New <span class='provider'>constituent</span> in <span class='provider'>Raiser's Edge NXT</span>",
+
       type: :paging_asc,
 
-      input_fields: ->() {
-        [{
+      input_fields: ->(_object_definitions) {
+        [
+          {
             name: 'since', type: :timestamp,
-            hint: 'Defaults to contituents created/updated after the recipe is first started'
-        }]
+            hint: 'Defaults to contituents created after the recipe is first started'
+          }
+        ]
       },
-      
-      poll: ->(connection, input, link) {
-        if link != nil
+
+      poll: ->(_connection, input, link) {
+        if link.present?
           response = get(link)
         else
-          params = {
-            'date_added' => (input['since'] || Time.now).to_time.utc.iso8601
-          }
-          response = get("https://api.sky.blackbaud.com/constituent/v1/constituents", params)
+          response = get("https://api.sky.blackbaud.com/constituent/v1/constituents").
+                     params(date_added: (input['since'] || Time.now).to_time.utc.iso8601,
+                            limit: PAGE_SIZE)
         end
+
+        constituents = response['value'].each do |constituent|
+                         birthdate = constituent['birthdate']
+
+                         if birthdate.present?
+                           constituent['birthdate'] = Date.new(birthdate['y'], birthdate['m'], birthdate['d'])
+                         end
+                       end
+
         {
-          events: response['value'],
+          events: constituents,
           next_poll: response['next_link'],
+          can_poll_more: constituents.length >= PAGE_SIZE
         }
       },
 
       dedup: ->(constituent) {
-        (constituent['id']).to_s + '-' + (constituent['date_added']).to_s
+        constituent['id']
       },
 
       output_fields: ->(object_definitions) {
         object_definitions['constituent']
       }
     },
-    
-    new_or_updated_constituent: {
-      type: :paging_asc,
 
-      input_fields: ->() {
-        [{
+    new_or_updated_constituent: {
+      description: "New or Updated <span class='provider'>constituent</span> in <span class='provider'>Raiser's Edge NXT</span>",
+
+      input_fields: ->(_object_definitions) {
+        [
+          {
             name: 'since', type: :timestamp,
             hint: 'Defaults to contituents created/updated after the recipe is first started'
-        }]
+          }
+        ]
       },
-      
-      poll: ->(connection, input, link) {
-        if link != nil
+
+      poll: ->(_connection, input, link) {
+        if link.present?
           response = get(link)
         else
-          params = {
-            'last_modified' => (input['since'] || Time.now).to_time.utc.iso8601
-          }
-          response = get("https://api.sky.blackbaud.com/constituent/v1/constituents", params)
+          response = get("https://api.sky.blackbaud.com/constituent/v1/constituents").
+                     params(last_modified: (input['since'] || Time.now).to_time.utc.iso8601,
+                            limit: PAGE_SIZE)
         end
+
+        constituents = response['value'].each do |constituent|
+                         birthdate = constituent['birthdate']
+
+                         if birthdate.present?
+                           constituent['birthdate'] = Date.new(birthdate['y'], birthdate['m'], birthdate['d'])
+                         end
+                       end
+
         {
-          events: response['value'],
+          events: constituents,
           next_poll: response['next_link'],
+          can_poll_more: constituents.length >= PAGE_SIZE
         }
       },
 
@@ -265,15 +242,26 @@
       output_fields: ->(object_definitions) {
         object_definitions['constituent']
       }
-    }, 
+    }
   },
 
   pick_lists: {
-    title: ->(connection) {
-        get("https://api.sky.blackbaud.com/constituent/v1/titles")['value'].
-          map { |title| [title, title] }},
-    suffix: ->(connection) {
-        get("https://api.sky.blackbaud.com/constituent/v1/suffixes")['value'].
-          map { |suffix| [suffix, suffix] }}
+    title: ->(_connection) {
+      get("https://api.sky.blackbaud.com/constituent/v1/titles")['value'].
+        map { |title| [title, title] }
+    },
+
+    suffix: ->(_connection) {
+      get("https://api.sky.blackbaud.com/constituent/v1/suffixes")['value'].
+        map { |suffix| [suffix, suffix] }
+    },
+
+    gender: ->(_connection) {
+      [
+        ["Male", "male"],
+        ["Female", "female"],
+        ["Unknown", "unknown"]
+      ]
+    }
   }
 }
