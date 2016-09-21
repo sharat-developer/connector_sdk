cebb2e3df220971bbdf129b2a9bcae0300baed4c
Harish Shetty <harish@workato.com>
2016-09-20 22:15:08 -0700

    Hound comments part 3

--- custom_auth/tsheets_connector.rb
+++ custom_auth/tsheets_connector.rb
@@ -1,27 +1,27 @@
 # Adds operations missing from the standard adapter.
 {
-  title: 'TSheets custom',
+  title: "TSheets custom",
 
   connection: {
     fields: [
       {
-        name: 'subdomain',
-        control_type: 'subdomain',
-        url: '.tsheets.com',
-        label: 'TSheets subdomain',
-        hint: 'Your TSheets sub domain name as found in your TSheets URL'
+        name: "subdomain",
+        control_type: "subdomain",
+        url: ".tsheets.com",
+        label: "TSheets subdomain",
+        hint: "Your TSheets sub domain name as found in your TSheets URL"
       },
       {
-        name: 'api_token',
-        label: 'API token',
+        name: "api_token",
+        label: "API token",
       },
     ],
 
     authorization: {
-      type: 'custom_auth',
+      type: "custom_auth",
 
       credentials: ->(connection) {
-        headers('Authorization': "Bearer #{connection['api_token']}")
+        headers("Authorization": "Bearer #{connection["api_token"]}")
       }
     }
   },
@@ -34,44 +34,45 @@
     query_timesheets: {
       input_fields: ->() {
         [
-          { name: 'start_date', type: :timestamp },
-          { name: 'end_date', type: :timestamp },
+          { name: "start_date", type: :timestamp },
+          { name: "end_date", type: :timestamp },
         ]
       },
 
       execute: ->(connection, input) {
         {
           timesheets: get(
-            "https://#{connection['subdomain']}.tsheets.com/api/v1/timesheets",
+            "https://#{connection["subdomain"]}.tsheets.com/api/v1/timesheets",
             {
-              start_date: input['start_date'].to_date.to_s,
-              end_date: input['end_date'].to_date.to_s,
+              start_date: input["start_date"].to_date.to_s,
+              end_date: input["end_date"].to_date.to_s,
             }
-          )['results']['timesheets'].values
+          )["results"]["timesheets"].values
         }
       },
 
       output_fields: ->(object_definitions) {
         [
-          { name: 'id', type: 'integer'},
-          { name: 'user_id', type: 'integer'},
-          { name: 'jobcode_id', type: 'integer'},
-          { name: 'start', type: 'timestamp'},
-          { name: 'end', type: 'timestamp'},
-          { name: 'duration', type: 'integer'},
-          { name: 'date', type: 'timestamp'},
-          { name: 'tz', type: 'integer'},
-          { name: 'tz_str', type: 'string'},
-          { name: 'type', type: 'string'},
-          { name: 'location', type: 'string'},
-          { name: 'on_the_clock', type: 'boolean'},
-          { name: 'locked', type: 'integer'},
-          { name: 'notes', type: 'string'},
-          { name: 'customfields',
-            type: 'object',
+          { name: "id", type: "integer"},
+          { name: "user_id", type: "integer"},
+          { name: "jobcode_id", type: "integer"},
+          { name: "start", type: "timestamp"},
+          { name: "end", type: "timestamp"},
+          { name: "duration", type: "integer"},
+          { name: "date", type: "timestamp"},
+          { name: "tz", type: "integer"},
+          { name: "tz_str", type: "string"},
+          { name: "type", type: "string"},
+          { name: "location", type: "string"},
+          { name: "on_the_clock", type: "boolean"},
+          { name: "locked", type: "integer"},
+          { name: "notes", type: "string"},
+          {
+            name: "customfields",
+            type: "object",
             properties: [
               # Add your custom fields here
-              # { name: "71138", label: 'location'},
+              # { name: "71138", label: "location"},
             ]
           }
         ]
