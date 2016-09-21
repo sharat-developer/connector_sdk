c6c16eadc0c20f9306c9cd615ee1caa4ae569440
Harish Shetty <harish@workato.com>
2016-09-20 23:42:46 -0700

    Hoind comments part 5

--- custom_auth/tsheets_connector.rb
+++ custom_auth/tsheets_connector.rb
@@ -20,59 +20,64 @@
     authorization: {
       type: "custom_auth",
 
-      credentials: ->(connection) do
+      credentials: lambda do
         headers("Authorization": "Bearer #{connection["api_token"]}")
       end
     }
   },
 
-  test: ->() {
+  test: lambda do
     true
-  },
+  end,
 
   actions: {
     query_timesheets: {
-      input_fields: ->() do
+      input_fields: lambda do
         [
           { name: "start_date", type: :timestamp },
           { name: "end_date", type: :timestamp },
         ]
       end,
 
-      execute: ->(connection, input) do
+      execute: lambda do |connection, input|
         {
           timesheets: get(
             "https://#{connection["subdomain"]}.tsheets.com/api/v1/timesheets",
-            {
-              start_date: input["start_date"].to_date.to_s,
-              end_date: input["end_date"].to_date.to_s,
-            }
+            start_date: input["start_date"].to_date.to_s,
+            end_date: input["end_date"].to_date.to_s,
           )["results"]["timesheets"].values
         }
       end,
 
-      output_fields: ->() do
+      output_fields: lambda do 
         [
-          { name: "id", type: "integer" },
-          { name: "user_id", type: "integer" },
-          { name: "jobcode_id", type: "integer" },
-          { name: "start", type: "timestamp" },
-          { name: "end", type: "timestamp" },
-          { name: "duration", type: "integer" },
-          { name: "date", type: "timestamp" },
-          { name: "tz", type: "integer" },
-          { name: "tz_str", type: "string" },
-          { name: "type", type: "string" },
-          { name: "location", type: "string" },
-          { name: "on_the_clock", type: "boolean" },
-          { name: "locked", type: "integer" },
-          { name: "notes", type: "string" },
           {
-            name: "customfields",
-            type: "object",
+            name: "timesheets",
+            type: "array",
+            of: "object",
             properties: [
-              # Add your custom fields here
-              # { name: "71138", label: "location" },
+              { name: "id", type: "integer" },
+              { name: "user_id", type: "integer" },
+              { name: "jobcode_id", type: "integer" },
+              { name: "start", type: "timestamp" },
+              { name: "end", type: "timestamp" },
+              { name: "duration", type: "integer" },
+              { name: "date", type: "timestamp" },
+              { name: "tz", type: "integer" },
+              { name: "tz_str", type: "string" },
+              { name: "type", type: "string" },
+              { name: "location", type: "string" },
+              { name: "on_the_clock", type: "boolean" },
+              { name: "locked", type: "integer" },
+              { name: "notes", type: "string" },
+              {
+                name: "customfields",
+                type: "object",
+                properties: [
+                  # Add your custom fields here
+                  # { name: "71138", label: "location" },
+                ]
+              }
             ]
           }
         ]
