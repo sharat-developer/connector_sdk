b1d618a5b48d79209793c99db8537fdf0d1150c0
Ee Shan <eeshansim@gmail.com>
2016-09-23 14:26:01 +0800

    hound fix

--- custom_auth/magento_connector.rb
+++ custom_auth/magento_connector.rb
@@ -1,44 +1,44 @@
 {
-  title: 'Magento',
+  title: "Magento",
 
   connection: {
 
     fields: [
       {
-        name: 'server',
-        label: 'Server domain',
+        name: "server",
+        label: "Server domain",
         optional: false,
-        hint: 'What is your server domain?'
+        hint: "What is your server domain?"
       },
       {
-        name: 'token',
-        control_type: 'password',
+        name: "token",
+        control_type: "password",
         optional: false,
-        hint: 'Find out how to generate a token <a target="_blank" href="http://devdocs.magento.com/guides/v2.1//get-started/authentication/gs-authentication-token.html">here</a>'
+        hint: "Find out how to generate a token <a target="_blank" href="http://devdocs.magento.com/guides/v2.1//get-started/authentication/gs-authentication-token.html">here</a>"
       }
     ],
     
     authorization: {
-      type: 'custom',
+      type: "custom",
 
       credentials: lambda do |connection|
-        headers("Authorization": "Bearer #{connection['token']}")
+        headers("Authorization": "Bearer #{connection["token"]}")
       end
     }
   },
   
   test: lambda do |connection|
-    get("#{connection['server']}rest/default/V1/attributeMetadata/customer")
+    get("#{connection["server"]}rest/default/V1/attributeMetadata/customer")
   end,
 
   object_definitions: {
     customer: {
       fields: lambda do
         [
-          { name: 'id', type: :integer },
-          { name: 'name' },
-          { name: 'email' },
-          { name: 'created_at', type: :timestamp }
+          { name: "id", type: :integer },
+          { name: "name" },
+          { name: "email" },
+          { name: "created_at", type: :timestamp }
           # additional fields here
         ]
       end
@@ -47,11 +47,11 @@
     order: {
       fields: lambda do
         [
-          { name: 'customer_id', type: :integer },
-          { name: 'subtotal', type: :decimal },
-          { name: 'entity_id' },
-          { name: 'created_at', type: :timestamp },
-          { name: 'updated_at', type: :timestamp }
+          { name: "customer_id", type: :integer },
+          { name: "subtotal", type: :decimal },
+          { name: "entity_id" },
+          { name: "created_at", type: :timestamp },
+          { name: "updated_at", type: :timestamp }
           # additional fields here
         ]
       end 
@@ -60,233 +60,233 @@
 
   triggers: {
     new_customer: {
-      description: 'New <span class="provider">customer</span> in <span class="provider">Magento</span>',
+      description: "New <span class="provider">customer</span> in <span class="provider">Magento</span>",
 
       type: :paging_desc,
       
       input_fields: lambda do |object_definition|
-        object_definition['customer'].
-          only('created_at').
-          required('created_at')
+        object_definition["customer"].
+          only("created_at").
+          required("created_at")
       end,
       
       poll: lambda do |connection, input, page|
         page_size = 100
 
         param = {
-          'searchCriteria' => {
-            'filterGroups' => {
-              '0' => {
-                'filters' => {
-                  '0' => {
-                    'field' => 'created_at',
-                    'value' => "#{input['created_at'].to_time.utc.iso8601}",
-                    'conditionType' => 'gt'
+          "searchCriteria" => {
+            "filterGroups" => {
+              "0" => {
+                "filters" => {
+                  "0" => {
+                    "field" => "created_at",
+                    "value" => "#{input["created_at"].to_time.utc.iso8601}",
+                    "conditionType" => "gt"
                   }
                 }
               }
             },
-            'sortOrders' => {
-              '0' => {
-                'field' => 'created_at',
-                'direction' => 'desc'
+            "sortOrders" => {
+              "0" => {
+                "field" => "created_at",
+                "direction" => "desc"
               }
             }
           },
-          'pageSize' => page_size,
-          'currentPage' => (page || 0)
+          "pageSize" => page_size,
+          "currentPage" => (page || 0)
         }
         
-        response = get("#{connection['server']}/rest/V1/customers/search", param)
+        response = get("#{connection["server"]}/rest/V1/customers/search", param)
         
-        next_page = if response['page_size'].blank? || response['page_size'] < page_size
+        next_page = if response["page_size"].blank? || response["page_size"] < page_size
                       0
                     else
-                      response['current_page'] + 1
+                      response["current_page"] + 1
                     end
 
         {
-          events: response['items'],
+          events: response["items"],
           next_page: next_page
         }
       end,
       
       output_fields: lambda do |object_definition|
-        object_definition['customer']
+        object_definition["customer"]
       end,
 
       sample_output: lambda do |connection|
         param = {
-          'searchCriteria' => {
-            'sortOrders' => {
-              '0' => {
-                'field' => 'created_at',
-                'direction' => 'desc'
+          "searchCriteria" => {
+            "sortOrders" => {
+              "0" => {
+                "field" => "created_at",
+                "direction" => "desc"
               }
             }
           },
-          'pageSize' => 1,
+          "pageSize" => 1,
         }
 
-        get("#{connection['server']}/rest/V1/customers/search", param)['items'].first || {}
+        get("#{connection["server"]}/rest/V1/customers/search", param)["items"].first || {}
       end
     },
     
     new_purchase_order: {
-      description: 'New <span class="provider">purchase order</span> in <span class="provider">Magento</span>',
+      description: "New <span class="provider">purchase order</span> in <span class="provider">Magento</span>",
 
       type: :paging_desc,
       
       input_fields: lambda do |object_definition|
-        object_definition['order'].
-          only('created_at').
-          required('created_at')
+        object_definition["order"].
+          only("created_at").
+          required("created_at")
       end,
       
       poll: lambda do |connection, input, page|
         page_size = 100
 
         param = {
-          'searchCriteria' => {
-            'filterGroups' => {
-              '0' => {
-                'filters' => {
-                  '0' => {
-                    'field' => 'created_at',
-                    'value' => "#{input['created_at'].to_time.utc.iso8601}",
-                    'conditionType' => 'gt'
+          "searchCriteria" => {
+            "filterGroups" => {
+              "0" => {
+                "filters" => {
+                  "0" => {
+                    "field" => "created_at",
+                    "value" => "#{input["created_at"].to_time.utc.iso8601}",
+                    "conditionType" => "gt"
                   }
                 }
               }
             },
-            'sortOrders' => {
-              '0' => {
-                'field' => 'created_at',
-                'direction' => 'desc'
+            "sortOrders" => {
+              "0" => {
+                "field" => "created_at",
+                "direction" => "desc"
               }
             }
           },
-          'pageSize' => page_size,
-          'currentPage' => (page || 0)
+          "pageSize" => page_size,
+          "currentPage" => (page || 0)
         }
 
-        response = get("#{connection['server']}/rest/V1/orders", param)
+        response = get("#{connection["server"]}/rest/V1/orders", param)
 
-        next_page = if response['page_size'].blank? || response['page_size'] < page_size
+        next_page = if response["page_size"].blank? || response["page_size"] < page_size
                       0
                     else
-                      response['current_page'].to_i + 1
+                      response["current_page"].to_i + 1
                     end
 
         {
-          events: response['items'],
+          events: response["items"],
           next_page: next_page
         }
       end,
   
       document_id: lambda do |order|
-        order['entity_id']
+        order["entity_id"]
       end,
       
       output_fields: lambda do |object_definition|
-        object_definition['order']
+        object_definition["order"]
       end,
 
       sample_output: lambda do |connection|
         param = {
-          'searchCriteria' => {
-            'sortOrders' => {
-              '0' => {
-                'field' => 'created_at',
-                'direction' => 'desc'
+          "searchCriteria" => {
+            "sortOrders" => {
+              "0" => {
+                "field" => "created_at",
+                "direction" => "desc"
               }
             }
           },
-          'pageSize' => 1,
+          "pageSize" => 1,
         }
 
-        get("#{connection['server']}/rest/V1/orders", param)['items'].first || {}
+        get("#{connection["server"]}/rest/V1/orders", param)["items"].first || {}
       end
     },
 
     new_or_updated_purchase_order: {
-      description: 'New/Updated <span class="provider">purchase order</span> in <span class="provider">Magento</span>',
+      description: "New/Updated <span class="provider">purchase order</span> in <span class="provider">Magento</span>",
 
       type: :paging_desc,
       
       input_fields: lambda do |object_definition|
-        object_definition['order'].
-          only('updated_at').
-          required('updated_at')
+        object_definition["order"].
+          only("updated_at").
+          required("updated_at")
       end,
       
       poll: lambda do |connection, input, page|
         page_size = 100
 
         param = {
-          'searchCriteria' => {
-            'filterGroups' => {
-              '0' => {
-                'filters' => {
-                  '0' => {
-                    'field' => 'updated_at',
-                    'value' => "#{input['updated_at'].to_time.utc.iso8601}",
-                    'conditionType' => 'gt'
+          "searchCriteria" => {
+            "filterGroups" => {
+              "0" => {
+                "filters" => {
+                  "0" => {
+                    "field" => "updated_at",
+                    "value" => "#{input["updated_at"].to_time.utc.iso8601}",
+                    "conditionType" => "gt"
                   }
                 }
               }
             },
-            'sortOrders' => {
-              '0' => {
-                'field' => 'updated_at',
-                'direction' => 'desc'
+            "sortOrders" => {
+              "0" => {
+                "field" => "updated_at",
+                "direction" => "desc"
               }
             }
           },
-          'pageSize' => page_size,
-          'currentPage' => (page || 0)
+          "pageSize" => page_size,
+          "currentPage" => (page || 0)
         }
 
-        response = get("#{connection['server']}/rest/V1/orders", param)
+        response = get("#{connection["server"]}/rest/V1/orders", param)
 
-        next_page = if response['page_size'].blank? || response['page_size'] < page_size
+        next_page = if response["page_size"].blank? || response["page_size"] < page_size
                       0
                     else
-                      response['current_page'].to_i + 1
+                      response["current_page"].to_i + 1
                     end
 
         {
-          events: response['items'],
+          events: response["items"],
           next_page: next_page
         }
       end,
 
       document_id: lambda do |order|
-        order['entity_id']
+        order["entity_id"]
       end,
 
       sort_by: lambda do |order|
-        order['updated_at']
+        order["updated_at"]
       end,
 
       output_fields: lambda do |object_definition|
-        object_definition['order']
+        object_definition["order"]
       end,
 
       sample_output: lambda do |connection|
         param = {
-          'searchCriteria' => {
-            'sortOrders' => {
-              '0' => {
-                'field' => 'created_at',
-                'direction' => 'desc'
+          "searchCriteria" => {
+            "sortOrders" => {
+              "0" => {
+                "field" => "created_at",
+                "direction" => "desc"
               }
             }
           },
-          'pageSize' => 1,
+          "pageSize" => 1,
         }
 
-        get("#{connection['server']}/rest/V1/orders", param)['items'].first || {}
+        get("#{connection["server"]}/rest/V1/orders", param)["items"].first || {}
       end
     }
   }
