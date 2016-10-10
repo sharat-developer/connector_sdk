1ff956aec23227b4f94ab2566742004635a8c4e7
Harish Shetty <harish@workato.com>
2016-10-10 14:01:48 -0700

    Fixed formatting

--- basic_auth/splunk_connector.rb
+++ basic_auth/splunk_connector.rb
@@ -1,44 +1,42 @@
 {
-  title: 'Splunk',
+  title: "Splunk",
   secure_tunnel: true,
 
   connection: {
     fields: [
       {
-        name: 'server_url',
-        label: 'Server URL',
-        control_type: 'text',
-        hint: 'The URL of the Splunk management port (e.g. https://yourdomain:8089). You MUST install the <a href="https://splunkbase.splunk.com/apps/#/search/workato">Workato Add-on for Splunk</a> first.'
+        name: "server_url",
+        label: "Server URL",
+        control_type: "text",
+        hint: "The URL of the Splunk management port (e.g. https://yourdomain:8089). You MUST install the <a href=\"https://splunkbase.splunk.com/apps/#/search/workato\">Workato Add-on for Splunk</a> first."
       },
       {
-        name: 'username',
-        hint: 'The Splunk username (e.g. admin)'
+        name: "username",
+        hint: "The Splunk username (e.g. admin)"
       },
       {
-        name: 'password',
-        control_type: 'password',
-        hint: 'The password for the Splunk username'
+        name: "password",
+        control_type: "password",
+        hint: "The password for the Splunk username"
       }
     ],
     authorization: {
-      type: 'basic_auth',
+      type: "basic_auth",
       credentials: ->(connection) {
-        user(connection['username'])
-        password(connection['password'])
+        user(connection["username"])
+        password(connection["password"])
       }
     }
   },
 
   test: ->(connection) {
-      get("#{connection['server_url']}/services/workato/version")
+    get("#{connection['server_url']}/services/workato/version")
   },
 
   object_definitions: {
     generic_alert: {
       fields: ->(connection, config_fields) {
-        config_fields['fields'].split(",").map { |name| {
-          name: name.strip
-          }}
+        config_fields["fields"].split(",").map { |name| { name: name.strip }}
       }
     },
     service_alert: {
@@ -51,86 +49,105 @@
           { name: "severity_label", type: :string },
           { name: "description", type: :string },
         ]
-        }
+      }
     },
   },
 
   triggers: {
     new_generic_alert: {
-      input_fields: ->(object_definitions) {[{
-        name: 'search_name',
-        label: 'Splunk alert',
-        type: :string,
-        control_type: :select,
-        pick_list: 'saved_searches',
-        optional: false,
-        hint: "Select one of the alerts saved in Splunk that have the Workato alert action assigned.",
-      }]},
+      input_fields: ->(object_definitions) {
+        [
+          {
+            name: "search_name",
+            label: "Splunk alert",
+            type: :string,
+            control_type: :select,
+            pick_list: "saved_searches",
+            optional: false,
+            hint: "Select one of the alerts saved in Splunk that have the Workato alert action assigned.",
+          }
+        ]
+      },
+
       config_fields: [
         {
-          name: 'fields',
+          name: "fields",
           label: "Alert fields",
           type: :string,
           optional: false,
           hint: "Comma-separated field names to be taken over from the Splunk data (e.g. host, count)",
         }
       ],
+
       webhook_subscribe: ->(callback_url, connection, input, flow_id) {
         data = post(
-          "#{connection['server_url']}/services/workato/alerts",{
-          	callback_url: callback_url,
-          	search_name: input['search_name'],
-            })
+          "#{connection['server_url']}/services/workato/alerts",
+          callback_url: callback_url,
+          search_name: input["search_name"]
+        )
+
         {
-          server_url: connection['server_url'],
-          search_name: data['search_name'],
-          callback_url: data['callback_url'],
+          server_url: connection["server_url"],
+          search_name: data["search_name"],
+          callback_url: data["callback_url"],
         }
       },
+
       webhook_unsubscribe: ->(subscription) {
-        delete("#{subscription['server_url']}/services/workato/alerts",{
-          search_name: subscription['search_name'],
-          callback_url: subscription['callback_url'],
-          })
+        delete(
+          "#{subscription["server_url"]}/services/workato/alerts",
+          search_name: subscription["search_name"],
+          callback_url: subscription["callback_url"]
+        )
       },
+
       webhook_notification: ->(input, payload) {
         payload
       },
+
       dedup: ->(event) {
         rand()
-	    },
-      output_fields: ->(object_definitions) {
-        object_definitions['generic_alert']
       },
-  	},
+
+      output_fields: ->(object_definitions) {
+        object_definitions["generic_alert"]
+      },
+    },
+
     new_service_alert: {
       webhook_subscribe: ->(callback_url, connection, input, flow_id) {
         data = post(
-          "#{connection['server_url']}/services/workato/servicealerts",{
-          	callback_url: callback_url
-            })
+          "#{connection['server_url']}/services/workato/servicealerts",
+          callback_url: callback_url
+        )
+
         {
-          server_url: connection['server_url'],
-          search_name: data['search_name'],
-          callback_url: data['callback_url'],
+          server_url: connection["server_url"],
+          search_name: data["search_name"],
+          callback_url: data["callback_url"],
         }
       },
+
       webhook_unsubscribe: ->(subscription) {
-        delete("#{subscription['server_url']}/services/workato/servicealerts",{
-          search_name: subscription['search_name'],
-          callback_url: subscription['callback_url'],
-          })
+        delete(
+          "#{subscription['server_url']}/services/workato/servicealerts",
+          search_name: subscription["search_name"],
+          callback_url: subscription["callback_url"]
+        )
       },
+
       webhook_notification: ->(input, payload) {
         payload
       },
+
       dedup: ->(event) {
-        event['event_id']
-	    },
-      output_fields: ->(object_definitions) {
-        object_definitions['service_alert']
+        event["event_id"]
       },
-  	},
+
+      output_fields: ->(object_definitions) {
+        object_definitions["service_alert"]
+      },
+    },
   },
 
   pick_lists: {
@@ -139,5 +156,4 @@
         map { |name| [name,name] }
     }
   }
-
 }
