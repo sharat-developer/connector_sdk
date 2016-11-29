e6dff4948e351b0441672b69d290ef9d145436f3
Robert <->
2016-11-29 18:38:47 +0100

    Replaced single quotes, newlines, etc.

--- basic_auth/splunk_connector.rb
+++ basic_auth/splunk_connector.rb
@@ -1,29 +1,29 @@
 {
-  title: 'Splunk',
+  title: "Splunk",
   secure_tunnel: true,
   connection: {
     fields: [
       {
-        name: 'server_url',
-        label: 'Server URL',
-        control_type: 'text',
-        hint: 'The URL of the Splunk management port (e.g. https://yourdomain:8089). You MUST install the <a href="https://splunkbase.splunk.com/apps/#/search/workato">Workato Add-on for Splunk</a> first.'
+        name: "server_url",
+        label: "Server URL",
+        control_type: "text",
+        hint: "The URL of the Splunk management port (e.g. https://yourdomain:8089). You MUST install the <a href=\"https://splunkbase.splunk.com/apps/#/search/workato\">Workato Add-on for Splunk</a> first."
       },
       {
-        name: 'username',
-        hint: 'The Splunk username (e.g. admin)'
+        name: "username",
+        hint: "The Splunk username (e.g. admin)"
       },
       {
-        name: 'password',
-        control_type: 'password',
-        hint: 'The password for the Splunk username'
+        name: "password",
+        control_type: "password",
+        hint: "The password for the Splunk username"
       }
     ],
     authorization: {
-      type: 'basic_auth',
+      type: "basic_auth",
       credentials: lambda do |connection|
-        user(connection['username'])
-        password(connection['password'])
+        user(connection["username"])
+        password(connection["password"])
       end
     }
   },
@@ -33,7 +33,7 @@
   object_definitions: {
     generic_alert: {
       fields: lambda do |_connection, config_fields|
-        config_fields['fields'].split(",").map { |name| {
+        config_fields["fields"].split(",").map { |name| {
           name: name.strip
           }}
       end
@@ -56,53 +56,55 @@
     new_generic_alert: {
       input_fields: lambda do |_object_definitions|
         [{
-          name: 'search_name',
-          label: 'Splunk alert',
+          name: "search_name",
+          label: "Splunk alert",
           type: :string,
           control_type: :select,
-          pick_list: 'saved_searches',
+          pick_list: "saved_searches",
           optional: false,
-          hint: "Select one of the alerts saved in Splunk that have the Workato alert action assigned.",
+          hint: "Select one of the alerts saved in Splunk that have the Workato " \
+            "alert action assigned.",
         }]
       end,
       config_fields: [
         {
-          name: 'fields',
+          name: "fields",
           label: "Alert fields",
           type: :string,
           optional: false,
-          hint: "Comma-separated field names to be taken over from the Splunk data (e.g. host, count)",
+          hint: "Comma-separated field names to be taken over from the Splunk" \
+            "data (e.g. host, count)",
         }
       ],
       webhook_subscribe: lambda do |callback_url, connection, input, _flow_id|
         data = post(
           "#{connection['server_url']}/services/workato/alerts",
           callback_url: callback_url,
-          search_name: input['search_name']
+          search_name: input["search_name"]
         )
         {
-          server_url: connection['server_url'],
-          search_name: data['search_name'],
-          callback_url: data['callback_url']
+          server_url: connection["server_url"],
+          search_name: data["search_name"],
+          callback_url: data["callback_url"]
         }
       end,
       webhook_unsubscribe: lambda do |subscription|
         delete(
           "#{subscription['server_url']}/services/workato/alerts",
-          search_name: subscription['search_name'],
-          callback_url: subscription['callback_url']
+          search_name: subscription["search_name"],
+          callback_url: subscription["callback_url"]
         )
       end,
       webhook_notification: lambda do |_input, payload|
         payload
       end,
-      dedup: lambda do |event|
+      dedup: lambda do |_event|
         rand()
-	    end,
+      end,
       output_fields: lambda do |object_definitions|
-        object_definitions['generic_alert']
+        object_definitions["generic_alert"]
       end
-  	},
+    },
     new_service_alert: {
       webhook_subscribe: lambda do |callback_url, connection, _input, _flow_id|
         data = post(
@@ -110,28 +112,28 @@
           callback_url: callback_url
         )
         {
-          server_url: connection['server_url'],
-          search_name: data['search_name'],
-          callback_url: data['callback_url'],
+          server_url: connection["server_url"],
+          search_name: data["search_name"],
+          callback_url: data["callback_url"],
         }
       end,
       webhook_unsubscribe: lambda do |subscription|
         delete(
           "#{subscription['server_url']}/services/workato/servicealerts",
-          search_name: subscription['search_name'],
-          callback_url: subscription['callback_url'],
+          search_name: subscription["search_name"],
+          callback_url: subscription["callback_url"],
         )
       end,
       webhook_notification: lambda do |_input, payload|
         payload
       end,
       dedup: lambda do |event|
-        event['event_id']
-	    end,
+        event["event_id"]
+      end,
       output_fields: lambda do |object_definitions|
-        object_definitions['service_alert']
+        object_definitions["service_alert"]
       end
-  	},
+    },
   },
   pick_lists: {
     saved_searches: lambda do |connection|
@@ -155,21 +157,23 @@
           {
             name: "source",
             optional: true,
-            hint: "The source value to assign to the event data. For example, if you're sending data from an " \
-              "app you're developing, you could set this key to the name of the app."
+            hint: "The source value to assign to the event data. For example, if " \
+              "you're sending data from an app you're developing," \
+              "you could set this key to the name of the app."
           },
           {
             name: "sourcetype",
             optional: true,
-            hint: "The sourcetype value to assign to the event data. It identifies the data structure of an event. " \
-              "A source type determines how Splunk formats the data during the indexing and also parses the data " \
-              "during searching process."
+            hint: "The sourcetype value to assign to the event data. It identifies " \
+              "the data structure of an event. " \
+              "A source type determines how Splunk formats the data during the" \
+              "indexing and also parses the data during searching process."
           },
           {
             name: "host",
             optional: true,
-            hint: "The host value to assign to the event data. This is typically the hostname of the " \
-              "client/server/service from which the data came from."
+            hint: "The host value to assign to the event data. This is typically the " \
+              "hostname of the client/server/service from which the data came from."
           },
         ]
       end,
